65e1e1c8fddb6ff55e5e926073d245f63c3ddbb6
rushin236 <rushikeshshindesandesh@gmail.com>
2024-01-22 22:53:03 +0530

    merged home page correction into main

--- Home.py
+++ Home.py
@@ -9,7 +9,7 @@ def main():
     ### About Project
 
     - This is a app for building regression and classification models.
-    - Build with the help of python and steamlit api.
+    - Build with the help of python and streamlit api.
     - Where you can upload your csv or excel file.
     - Choose the page for the model you want to build regression or classification.
     - Choose the model us want to build or explore all the models and see for your self which suites your dataset the most.
@@ -17,11 +17,11 @@ def main():
 
     ### Features
 
-    - Included variety of models for classfication and regression.
+    - Included variety of models for classification and regression.
     - Included hyperparameter tuning so user can choose for him self the best parameters for data.
-    - Inclused model performance report for train and test data.
+    - Included model performance report for train and test data.
     - Also added pandas profiling for data analysis.
-    - Also added data transformation feature for those who want to do standarScaling, minmax scaling and logtransformation.
+    - Also added data transformation feature for those who want to do standerScaling, minmax scaling and log transformation.
     - Downloading model with scaler for future predictions.
     """
     )
